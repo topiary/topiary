2c18c60d2053bb46be6052d5101919ebf6a25cb9
Philipp Zander <philipp.zander@tweag.io>
2026-04-30 04:10:03 +0200

    fix(core): `multi_line_indent_all` should consider difference of indentation instead of position

--- topiary-core/src/pretty.rs
+++ topiary-core/src/pretty.rs
@@ -48,7 +48,7 @@ pub fn render(atoms: &[Atom], indent: &str) -> FormatterResult<String> {
 
             Atom::Leaf {
                 content,
-                original_position,
+                original_indentation,
                 single_line_no_indent,
                 multi_line_indent_all,
                 keep_whitespace,
@@ -67,12 +67,9 @@ pub fn render(atoms: &[Atom], indent: &str) -> FormatterResult<String> {
                 };
 
                 let mut content = if *multi_line_indent_all {
-                    let cursor = current_column(&buffer) as i32;
+                    let cursor = crate::atom_collection::count_indenting_characters(&buffer) as i32;
 
-                    // original_position is 1-based
-                    let original_column = original_position.column as i32 - 1;
-
-                    let indenting = cursor - original_column;
+                    let indenting = cursor - *original_indentation as i32;
 
                     // The following assumes spaces are used for indenting
                     match indenting {
@@ -110,10 +107,6 @@ pub fn render(atoms: &[Atom], indent: &str) -> FormatterResult<String> {
     }
 
     Ok(buffer)
-}
-
-fn current_column(s: &str) -> usize {
-    s.chars().rev().take_while(|c| *c != '\n').count()
 }
 
 fn add_spaces_after_newlines(s: &str, n: i32) -> String {
